5953d7f143bb991dedc39921343c29b70916d3d0
Sanchit Vasdev <sanchitvasdev@gmail.com>
2024-04-14 23:41:48 +0530

    experience animation fix

--- components/experience.tsx
+++ components/experience.tsx
@@ -15,51 +15,51 @@ import { useInView } from "react-intersection-observer";
 export default function Experience() {
   const { ref } = useSectionInView("Experience", 0.4);
   const { theme } = useTheme();
+  const { ref: inViewRef, inView } = useInView({ triggerOnce: true });
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
-        {experiencesData.map((item, index) => {
-          const { ref, inView } = useInView({
-            triggerOnce: true,
-          });
-          return (
-            <div key={index} ref={ref} className="vertical-timeline-element">
-              <VerticalTimelineElement
-                contentStyle={{
-                  background:
-                    theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
-                  boxShadow: "none",
-                  border: "1px solid rgba(0, 0, 0, 0.05)",
-                  textAlign: "left",
-                  padding: "1.3rem 2rem",
-                }}
-                contentArrowStyle={{
-                  borderRight:
-                    theme === "light"
-                      ? "0.4rem solid #9ca3af"
-                      : "0.4rem solid rgba(255,255,255,0.5)",
-                }}
-                visible={inView}
-                date={item.date}
-                icon={item.icon}
-                iconStyle={{
-                  background:
-                    theme === "light" ? "white" : "rgba(255,255,255,0.15)",
-                  fontSize: "1.5rem",
-                }}
-              >
-                <h3 className="font-semibold capitalize">{item.title}</h3>
-                <p className="font-normal !mt-0">{item.location}</p>
-                <ul className="!mt-1 !font-normal text-gray-700 list-disc dark:text-white/75">
-                  {item.description.map((descr, index) => (
-                    <li key={index}>{descr}</li>
-                  ))}
-                </ul>
-              </VerticalTimelineElement>
-            </div>
-          );
-        })}
+        <div ref={inViewRef}>
+          {experiencesData.map((item, index) => {
+            return (
+              <div key={index} className="vertical-timeline-element">
+                <VerticalTimelineElement
+                  contentStyle={{
+                    background:
+                      theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
+                    boxShadow: "none",
+                    border: "1px solid rgba(0, 0, 0, 0.05)",
+                    textAlign: "left",
+                    padding: "1.3rem 2rem",
+                  }}
+                  contentArrowStyle={{
+                    borderRight:
+                      theme === "light"
+                        ? "0.4rem solid #9ca3af"
+                        : "0.4rem solid rgba(255,255,255,0.5)",
+                  }}
+                  visible={inView}
+                  date={item.date}
+                  icon={item.icon}
+                  iconStyle={{
+                    background:
+                      theme === "light" ? "white" : "rgba(255,255,255,0.15)",
+                    fontSize: "1.5rem",
+                  }}
+                >
+                  <h3 className="font-semibold capitalize">{item.title}</h3>
+                  <p className="font-normal !mt-0">{item.location}</p>
+                  <ul className="!mt-1 !font-normal text-gray-700 list-disc dark:text-white/75">
+                    {item.description.map((descr, index) => (
+                      <li key={index}>{descr}</li>
+                    ))}
+                  </ul>
+                </VerticalTimelineElement>
+              </div>
+            );
+          })}
+        </div>
       </VerticalTimeline>
     </section>
   );
