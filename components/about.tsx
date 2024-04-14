02eeb99d5594d6dc3f2122318cc0cabd3d7133aa
Sanchit Vasdev <sanchitvasdev@gmail.com>
2024-04-14 22:59:09 +0530

    changes

--- components/about.tsx
+++ components/about.tsx
@@ -40,10 +40,10 @@ export default function About() {
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
+        <span className="italic">When I am not coding</span>, I enjoy playing
         chess, watching movies, and playing cricket. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
-        learning about <span className="font-medium">stock market</span>. I'm
+        learning about <span className="font-medium">stock market</span>. I am
         also learning how to play the guitar.
       </p>
     </motion.section>
--- components/intro.tsx
+++ components/intro.tsx
@@ -57,7 +57,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Sanchit.</span> I'm a{" "}
+        <span className="font-bold">Hello, I am Sanchit.</span> I am a{" "}
         <span className="font-bold">Full-stack web developer</span> with{" "}
         <span className="font-bold">1 year</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
